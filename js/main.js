89ebe94e1a031d421a7f49c2bb4ed890fba21ffb
MunKyeongGu <kgmoon98@hanmail.net>
2022-04-26 16:41:28 +0900

    전체삭제 추가
    전체삭제 추가

--- js/main.js
+++ js/main.js
@@ -1,31 +1,18 @@
 const TODO_INPUT = document.getElementById("todoInput");
 const TODO_SUBMIT_BUTTON = document.getElementById("todoCreate");
 const TODO_WRAP = document.querySelector(".todo-main ul");
+const TODO_ALL_DELETE = document.querySelector(".all-delete");
+let isRun = false;
 
 function todoAdd(){
     if(TODO_INPUT.value == "") {
         alert("할 일을 입력해주세요.");
     } else{
         let todoText = TODO_INPUT.value;
-        addTesk(todoText);
+        addEvent(todoText);
     }
 }
 
-function addTesk(value) {
-    const todoItem = document.createElement("li");
-    todoItem.classList.add("todo-box");
-    todoItem.innerHTML = `<label for="todo-check"></label><input type="checkbox" class="todo-check"><p>${value}</p><button class="todo-delete">삭제</button>`;
-    TODO_WRAP.appendChild(todoItem);
-    TODO_INPUT.value = "";
-    deleteEvent();
-    checkEvent();
-
-    let todoItemLength = document.getElementsByClassName("todo-box").length;
-    if(todoItemLength >= 1) {
-        const NO_DATA = document.querySelector(".no-data");
-        NO_DATA.remove();
-    }
-}
 
 function todoDelete(e) {
     const DELETE_ELEMENT = e.target.parentNode;
@@ -38,6 +25,8 @@ function todoDelete(e) {
 
     if(todoItemLength == 0) {
         TODO_WRAP.appendChild(NO_DATA);
+
+        isRun = false;
     }
 }
 
@@ -48,6 +37,35 @@ function todoCheck(e) {
         CHECK_ELEMENT.classList.add("checked");
     } else {
         CHECK_ELEMENT.classList.remove("checked");
+    }
+}
+
+function todoAllDelete() {
+    TODO_WRAP.innerHTML = "";
+
+    const NO_DATA = document.createElement("li");
+    NO_DATA.classList.add("no-data");
+    NO_DATA.innerHTML = "데이터가 없습니다."
+    TODO_WRAP.append(NO_DATA);
+    isRun = false;
+}
+
+
+function addEvent(value) {
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("todo-box");
+    todoItem.innerHTML = `<label for="todo-check"></label><input type="checkbox" class="todo-check"><p class="todo-comment">${value}</p><button class="todo-delete">삭제</button>`;
+    TODO_WRAP.appendChild(todoItem);
+    TODO_INPUT.value = "";
+    deleteEvent();
+    checkEvent();
+
+    let todoItemLength = document.getElementsByClassName("todo-box").length;
+    if(todoItemLength >= 1 && isRun == false) {
+        const NO_DATA = document.querySelector(".no-data");
+        NO_DATA.remove();
+
+        isRun = true;
     }
 }
 
@@ -74,3 +92,4 @@ function enterKey() {
 }
 
 TODO_SUBMIT_BUTTON.addEventListener("click", todoAdd);
+TODO_ALL_DELETE.addEventListener("click", todoAllDelete);
